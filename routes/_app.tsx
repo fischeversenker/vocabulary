624f5b758c38b8ae4d9a850d8ad816dcc3821772
Felix Hamann <felix.hamann@satellytes.com>
2023-08-17 19:21:13 +0200

    remove footer import

--- routes/_app.tsx
+++ routes/_app.tsx
@@ -1,5 +1,4 @@
 import { AppProps } from "$fresh/server.ts";
-import { Footer } from "../components/Footer.tsx";
 import { Header } from "../components/Header.tsx";
 
 export default function App({ Component }: AppProps) {
