6dfafbd96a437b52f1ac1f5ebf4849653ab5d98b
Felix Hamann <felix.hamann@satellytes.com>
2023-08-17 19:13:42 +0200

    slight beautification

--- routes/index.tsx
+++ routes/index.tsx
@@ -7,7 +7,6 @@ interface Data {
 }
 
 interface Word {
-  id: string;
   word: string;
   translation: string;
 }
@@ -17,7 +16,7 @@ export const handler: Handlers<Data> = {
     const url = new URL(req.url);
     const word = url.searchParams.get("word");
     const translation = url.searchParams.get("translation");
-    const idToDelete = url.searchParams.get("idToDelete");
+    const wordToDelete = url.searchParams.get("wordToDelete");
 
     const kv = await Deno.openKv();
     const knownWordsKv = await kv.get<Word[]>(['words']);
@@ -25,10 +24,12 @@ export const handler: Handlers<Data> = {
     let newKnownWords: Word[] = [...knownWords];
 
     if (word && translation) {
-      const id = crypto.randomUUID();
-      newKnownWords.push({ id, word, translation });
-    } else if (idToDelete) {
-      newKnownWords = knownWords.filter((word) => word.id !== idToDelete);
+      if (knownWords.some((knownWord) => knownWord.word === word)) {
+        return ctx.render({ knownWords });
+      }
+      newKnownWords.push({ word, translation });
+    } else if (wordToDelete) {
+      newKnownWords = knownWords.filter((word) => word.word !== wordToDelete);
     }
     await kv.set(['words'], newKnownWords);
 
@@ -41,17 +42,19 @@ export default function Home({ data }: PageProps<Data>) {
 
   return (
     <div>
-      <p>Add new word:</p>
-      <form>
+      <p style="padding-bottom:1rem;">Add new word:</p>
+      <form style="display:flex;gap:0.5rem;">
         <input type="text" name="word" required />
         <input type="text" name="translation" required />
         <button type="submit">Add</button>
       </form>
-      <ul>
-        {knownWords.reverse().map((word) => (
-          <li key={word}>{word.word}: {word.translation} <form style="display:inline;"><input type="hidden" name="idToDelete" value={word.id} /><Button type="submit">X</Button></form></li>
-        ))}
-      </ul>
+      <div style="padding-top:1rem;">
+        <ul>
+          {knownWords.reverse().map((word) => (
+            <li key={word.word} style="display:flex;gap:0.5rem;"><span>{word.word}</span><i>{word.translation}</i><span><form style="display:inline;"><input type="hidden" name="wordToDelete" value={word.word} /><Button type="submit">X</Button></form></span></li>
+          ))}
+        </ul>
+      </div>
     </div>
   );
 }
